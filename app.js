650a7fc0e3d9b40782d15e6409e6689f3bebb84d
Sean Wildman <sean.wildman75@gmail.com>
2022-02-16 21:54:00 -0500

    add pseudocode comments to app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Rock, Paper, Scissors game for The Odin Project
+// Pseudocode =>
+// 1 - User chooses between (rock, paper or scisors) case insensitive
+// 2 - Computer will randomly choose between rock, paper, and Scissors
+// 3 - Compare user choice against computer choice
+// 4 - print out who won
+// 5 - print out times won marker
